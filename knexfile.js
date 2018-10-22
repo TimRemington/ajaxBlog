2773b1f1f0f778c640d9f43f0c84c684f60616ea
Tim Remington <timremington@Tims-MacBook-Pro.local>
2018-10-22 09:47:15 -0600

    getting ready for heroku

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,6 @@ module.exports = {
   test: {},
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: 'postgres://xwhhzqbtmtxmmz:d8287c4983ef03b702c533d79c6a885bf365d96cd761f4a2cb17b7ed352a5285@ec2-54-204-14-96.compute-1.amazonaws.com:5432/det80vg40r1t2j'
   }
 }
